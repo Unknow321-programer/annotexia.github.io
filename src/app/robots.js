2bff6a3d10eec5bb3227cc86402ffbaf7d952503
Saurabh <saurabhhgade@gmail.com>
2026-07-19 20:52:37 +0530

    allow bot to search results

--- src/app/robots.js
+++ src/app/robots.js
@@ -1,10 +1,48 @@
 export default function robots() {
   return {
-    rules: {
-      userAgent: "*",
-      allow: "/",
-    },
+    rules: [
+      {
+        userAgent: "OAI-SearchBot",
+        allow: "/",
+      },
+      {
+        userAgent: "ChatGPT-User",
+        allow: "/",
+      },
+      {
+        userAgent: "ClaudeBot",
+        allow: "/",
+      },
+      {
+        userAgent: "Claude-SearchBot",
+        allow: "/",
+      },
+      {
+        userAgent: "PerplexityBot",
+        allow: "/",
+      },
+      {
+        userAgent: "Google-Extended",
+        allow: "/",
+      },
+      {
+        userAgent: "CCBot",
+        allow: "/",
+      },
+      {
+        userAgent: "GoogleBot",
+        allow: "/",
+      },
+      {
+        userAgent: "Bingbot",
+        allow: "/",
+      },
 
+      {
+        userAgent: "*",
+        allow: "/",
+      },
+    ],
     sitemap: "https://www.annotexia.com/sitemap.xml",
   };
 }
